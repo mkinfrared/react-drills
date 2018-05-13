9fd43f229151d9c9f6974f865e477963d7a94deb
mkinfrared <mkinfrared@gmail.com>
2018-05-13 16:07:05 -0600

    completed task 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,21 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Image from './components/Image/Image';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			images: [],
+			url   : ''
+		};
+
+		this.handleClick  = this.handleClick.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+
+	}
+
+	handleClick() {
+		const {images, url} = this.state;
+		const key           = images.length;
+
+		this.setState({
+			images: [...images, <Image key={key} url={url}/>],
+			url   : ''
+		});
+	}
+
+	handleChange(ev) {
+		const {name, value} = ev.target;
+
+		this.setState({
+			[name]: value
+		});
+	}
+
+	render() {
+		const {images, url} = this.state;
+
+		return (
+			<div className="App">
+				<div>
+					<input onChange={(ev) => this.handleChange(ev)} type="text" name="url" placeholder="URL"
+						   value={url}/>
+					<button onClick={() => this.handleClick()}>Add image</button>
+				</div>
+				{images}
+			</div>
+		);
+	}
 }
 
 export default App;
